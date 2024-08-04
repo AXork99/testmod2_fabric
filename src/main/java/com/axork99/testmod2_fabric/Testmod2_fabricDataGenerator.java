809d42a2ef40d007735d2a2be8861cd126f70c0b
AXork99 <vilpelj@gmail.com>
2024-08-04 13:49:44 +0200

    Initial commit

---
+++ src/main/java/com/axork99/testmod2_fabric/Testmod2_fabricDataGenerator.java
@@ -0,0 +1,11 @@
+package com.axork99.testmod2_fabric;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class Testmod2_fabricDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
